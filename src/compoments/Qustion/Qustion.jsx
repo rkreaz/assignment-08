b51d386e2e56b4688cb5a997900aa7092843df00
Rk-Reaz <rkreaz91@gmail.com>
2023-04-01 03:12:18 +0600

    added question file

--- src/compoments/Qustion/Qustion.jsx
+++ src/compoments/Qustion/Qustion.jsx
@@ -1,50 +1,53 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Qustion.css'
 const Qustion = () => {
     return (
         <div className='container'>
             <div>
-            <Card>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
-                </Card.Body>
-            </Card><br />
-        </div>
-        
-            <div>
-            <Card>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
+               <Card>
+                 <Card.Body className='question_body'>
+                    <Card.Title className='question_title'>01: What is the difference between props and State...?</Card.Title>
+                    <Card.Text className='question_pra'>
+                    Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
                     </Card.Text>
                 </Card.Body>
             </Card><br />
         </div>
             <div>
-            <Card>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
+               <Card>
+                 <Card.Body className='question_body'>
+                    <Card.Title className='question_title'>02: What is the difference between props and State...?</Card.Title>
+                    <Card.Text className='question_pra'>
+                    Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
                     </Card.Text>
                 </Card.Body>
             </Card><br />
         </div>
             <div>
-            <Card>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
+               <Card>
+                 <Card.Body className='question_body'>
+                    <Card.Title className='question_title'>03: What is the difference between props and State...?</Card.Title>
+                    <Card.Text className='question_pra'>
+                    Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
+                    </Card.Text>
+                </Card.Body>
+            </Card><br />
+        </div>
+            <div>
+               <Card>
+                 <Card.Body className='question_body'>
+                    <Card.Title className='question_title'>04: What is the difference between props and State...?</Card.Title>
+                    <Card.Text className='question_pra'>
+                    Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
                     </Card.Text>
                 </Card.Body>
             </Card>
         </div>
+        
+          
+ 
         </div>
     );
 };
