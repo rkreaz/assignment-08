85ae8ec5c42eb7a935ba5073e9ec12722f3aa2d0
Rk-Reaz <rkreaz91@gmail.com>
2023-04-01 02:42:12 +0600

    added bookmarked section

--- src/compoments/Bookmarked/Bookmarked.jsx
+++ src/compoments/Bookmarked/Bookmarked.jsx
@@ -1,1 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './Bookmarked.css'
+const Bookmarked = ({booksmark, minBlog}) => {
+const [min, setMin] = useState(0)
 
+useEffect(()=>{
+    minBlog.map(m => {
+        setMin(min + m.readtime)
+    });
+},[minBlog])
+
+console.log(min)
+    return (
+        <div className='sidebar_list'>
+            <div className='sidebar-title'>
+                <h5 className='sidebar_min'>Spent time on read : {min} min</h5>
+            </div>
+            <div className='sidebar_blogs'>
+                    <h5 className='sidebar_blog'>Bookmarked Blogs : {booksmark.length}</h5>
+               
+            {booksmark.map(b => <div key={b.id} className='sidebar-list'>
+                <div className='bookmark-description'>
+                    <h3>{b.title}</h3>
+                </div>
+            </div>)}
+            </div>
+        </div>
+    );
+};
+
+export default Bookmarked;
