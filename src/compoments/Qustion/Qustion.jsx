f1f5428601007016ebf2074404a4611ed87ca5c8
Rk-Reaz <rkreaz91@gmail.com>
2023-04-01 02:39:01 +0600

    added qustion section

---
+++ src/compoments/Qustion/Qustion.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+const Qustion = () => {
+    return (
+        <div className='container'>
+            <div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                </Card.Body>
+            </Card><br />
+        </div>
+        
+            <div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                </Card.Body>
+            </Card><br />
+        </div>
+            <div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                </Card.Body>
+            </Card><br />
+        </div>
+            <div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+        </div>
+    );
+};
+
+export default Qustion;
